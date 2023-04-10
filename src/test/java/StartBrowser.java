3b9c31035aef2491e58e04633327a31e172ee164
kedarrnath <kedar.1224@gmail.com>
2023-04-10 11:29:36 +0530

    Signed-off-by: kedarrnath <kedar.1224@gmail.com>

---
+++ src/test/java/StartBrowser.java
@@ -0,0 +1,25 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class StartBrowser {
+    private static ChromeDriver chromeDriver;
+
+    @BeforeTest
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        chromeDriver = new ChromeDriver();
+    }
+
+    @Test
+    public void RunURL() {
+        chromeDriver.get("https://www.lambdatest.com/");
+    }
+
+    @AfterTest
+    public void crash() {
+        chromeDriver.close();
+    }
+}
