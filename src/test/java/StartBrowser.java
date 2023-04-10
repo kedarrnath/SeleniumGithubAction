6e560465a21ac7ae3cb5f46273407bba79849582
kedarrnath <kedar.1224@gmail.com>
2023-04-10 12:18:12 +0530

    - updated chrome options to run in headless mode
    Signed-off-by: kedarrnath <kedar.1224@gmail.com>

--- src/test/java/StartBrowser.java
+++ src/test/java/StartBrowser.java
@@ -1,6 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -11,7 +12,11 @@ public class StartBrowser {
     @BeforeTest
     public void setup() {
         WebDriverManager.chromedriver().setup();
-        chromeDriver = new ChromeDriver();
+        ChromeOptions chrome_options = new ChromeOptions();
+        chrome_options.addArguments("--headless");
+        chrome_options.addArguments("--no-sandbox");
+        chrome_options.addArguments("--disable-dev-shm-usage");
+        chromeDriver = new ChromeDriver(chrome_options);
     }
 
     @Test
